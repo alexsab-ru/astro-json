f9f7c2299b843bd4e80be03997766b02cdc5e145
Alexander Sabanov <alexsab@mail.ru>
2024-11-25 15:32:01 +0700

    add more sh file

---
+++ .github/scripts/sh/get-kaiyi-samara.sh
@@ -0,0 +1,5 @@
+export CSV_URL=$(grep '^CSV_URL=' .env | awk -F'=' '{print substr($0, index($0,$2))}' | sed 's/^"//; s/"$//')
+export QUERY_STRING="SELECT C, D, E, F WHERE A='Мега' and B='Kaiyi'"
+export KEY_COLUMN="Модель"
+export OUTPUT_PATHS="./src/kaiyi-samara.ru/data/dealer_price.json"
+node .github/scripts/getDealerData.js
