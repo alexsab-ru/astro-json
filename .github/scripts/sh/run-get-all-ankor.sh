7a47602b3314a579634eed2b9287309c5b4fd0c9
Alexander Sabanov <alexsab@mail.ru>
2025-02-12 19:42:12 +0700

    upd env variables

--- .github/scripts/sh/run-get-all-ankor.sh
+++ .github/scripts/sh/run-get-all-ankor.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-fi
-export CSV_URL=$CSV_URL_ALPHA
-# pnpm run get-belgee-samara
-# pnpm run get-jac-samara
-# pnpm run get-jetour-alpha
-# pnpm run get-livan-alpha
-# pnpm run get-kaiyi-alpha
-# pnpm run get-kaiyi-samara
-# pnpm run get-baic-alpha
+pnpm run get-belgee-samara
+pnpm run get-jac-samara
+pnpm run get-jetour-alpha
+pnpm run get-livan-alpha
+pnpm run get-kaiyi-alpha
+pnpm run get-kaiyi-samara
+pnpm run get-baic-alpha
 pnpm run get-baic-samara
--- .github/scripts/sh/run-get-all-krasnodar.sh
+++ .github/scripts/sh/run-get-all-krasnodar.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-fi
-export CSV_URL=$CSV_URL_AUTOHOLDING
 pnpm run get-jetour-krasnodar
 pnpm run get-kaiyi-krd
 pnpm run get-baic-krasnodar
