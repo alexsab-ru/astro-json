bc4de511de4477c47fd4005cefc47b409c6e0250
Alexander Sabanov <alexsab@mail.ru>
2025-03-11 21:07:53 +0700

    upd scrape workflow

--- .github/scripts/sh/scrape-geely.sh
+++ .github/scripts/sh/scrape-geely.sh
@@ -6,4 +6,7 @@ export MODEL_XPATH="./div/h4[@class='v2-automobile-menu-card__heading']/text()"
 export PRICE_XPATH="translate(string(./div/p[@class='v2-automobile-menu-card__paragraph']/text()), translate(string(./div/p[@class='v2-automobile-menu-card__paragraph']/text()), '0123456789', ''), '')"
 export LINK_XPATH="./@href"
 export OUTPUT_PATHS="./src/geely-partner-orenburg.ru/data/cars.json,./src/geely-partner-samara.ru/data/cars.json,./src/geely-partner-saratov.ru/data/cars.json,./src/geely-partner-vostok.ru/data/cars.json,./tmp/auto-team.pro/data/geely.json"
+export DEALERPRICE='dealer_price.json'
+export DEALERPRICEFIELD='Конечная цена'
+export DEALERBENEFITFIELD='Скидка'
 node .github/scripts/scrape.js
