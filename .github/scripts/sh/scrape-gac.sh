cca74517453ec5b9002de517fef77686930491e8
Alexander Sabanov <alexsab@mail.ru>
2025-03-21 18:14:13 +0700

    upd scrape sh

--- .github/scripts/sh/scrape-gac.sh
+++ .github/scripts/sh/scrape-gac.sh
@@ -5,5 +5,5 @@ export ID_XPATH="concat('gac-', substring-after(.//a[starts-with(@href, '/models
 export MODEL_XPATH="substring-after(.//a[starts-with(@href, '/models/')]/@href, '/models/')"
 export PRICE_XPATH="translate(string(.//span[@class='price']/text()), translate(string(.//span[@class='price']/text()), '0123456789', ''), '')"
 export LINK_XPATH=".//a[starts-with(@href, '/models/')]/@href"
-export OUTPUT_PATHS="./src/gac-smr.alexsab.ru/data/cars.json,./src/gac-orenburg.alexsab.ru/data/cars.json,./src/gac-stavauto.alexsab.ru/data/cars.json,./src/gac-partner-samara.alexsab.ru/data/cars.json,./tmp/auto-team.pro/data/gac.json"
+export OUTPUT_PATHS="./src/gac.alexsab.ru/data/cars.json,./src/gac-smr.alexsab.ru/data/cars.json,./src/gac-orenburg.alexsab.ru/data/cars.json,./src/gac-stavauto.alexsab.ru/data/cars.json,./src/gac-partner-samara.alexsab.ru/data/cars.json,./tmp/auto-team.pro/data/gac.json"
 node .github/scripts/scrape.js
