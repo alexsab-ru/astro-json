15ec84cb5a97eb54770564522a56c78d3e662074
Alexander Sabanov <alexsab@mail.ru>
2025-04-22 13:33:33 +0700

    use scrape py in wey

--- .github/scripts/sh/scrape-wey.sh
+++ .github/scripts/sh/scrape-wey.sh
@@ -1,12 +1,12 @@
 export BRAND='wey'
 export URL="https://gwm-wey.ru/"
-export ITEM_XPATH='//*[@id="car-variants"]//div[contains(@class, "swiper-slide")]'
-export ID_XPATH="concat('', substring-after(.//a[starts-with(@href, '/configurator/')]/@href, '/configurator/'))"
+export ITEM_XPATH="//*[@id='car-variants']//div[contains(@class, 'swiper-slide')]"
+export ID_XPATH="replace((.//a[starts-with(@href, '/models/')]/@href)[1], '^.*(?:/models/|variant_slug=)', '')"
 export MODEL_XPATH=".//article[@class='title']/p/text()"
-export PRICE_XPATH="translate(string(.//article[@class='price']/p/text()), translate(string(.//article[@class='price']/p/text()), '0123456789', ''), '')"
+export PRICE_XPATH="translate(string(.//article[@class='price']/p/text()[1]), translate(string(.//article[@class='price']/p/text()[1]), '0123456789', ''), '')"
 export LINK_XPATH="concat('models/', substring-after(.//a[starts-with(@href, '/configurator/')]/@href, '/configurator/'))"
-export OUTPUT_PATHS="./src/wey-penza.ru/data/cars.json"
+export OUTPUT_PATHS="./src/wey-penza.ru/data/cars.json,./src/wey.alexsab.ru/data/cars.json"
 export DEALERPRICE='dealer_price.json'
 export DEALERPRICEFIELD='Конечная цена'
 export DEALERBENEFITFIELD='Скидка'
-node .github/scripts/scrape.js
+python3 .github/scripts/scrape.py
