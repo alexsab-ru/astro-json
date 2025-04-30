ad86926450826613002b4b95a06baad49b1dd768
Alexander Sabanov <alexsab@mail.ru>
2025-04-30 09:35:26 +0400

    upd chery

--- .github/scripts/sh/scrape-chery.sh
+++ .github/scripts/sh/scrape-chery.sh
@@ -1,8 +1,8 @@
 export BRAND='chery'
 export URL="https://www.chery.ru/models/"
 export ITEM_XPATH="//div[contains(concat(' ', normalize-space(@class), ' '), ' js-menu-models-desc menu-models__desc-wrap ')]"
-export ID_XPATH="concat('chery-', substring-before(substring-after(.//div[contains(@class, 'menu-models__desc-btns')]/a[2]/@href, '/models/'), '/'))"
-export MODEL_XPATH="substring-before(substring-after(.//div[contains(@class, 'menu-models__desc-btns')]/a[2]/@href, '/models/'), '/')"
+export ID_XPATH="concat('chery-', substring-before(substring-after(.//div[contains(@class, 'menu-models__desc-btns')]/a[@href[starts-with(.,'/models/')]]/@href, '/models/'), '/'))"
+export MODEL_XPATH=".//div[contains(concat(' ', normalize-space(@class), ' '), ' menu-models__desc-title ')]"
 export PRICE_XPATH="translate(string(.//div[contains(@class, 'menu-models__desc-price')]/text()), translate(string(.//div[contains(@class, 'menu-models__desc-price')]/text()), '0123456789', ''), '')"
 export LINK_XPATH=".//div[contains(@class, 'menu-models__desc-btns')]/a[2]/@href"
 export OUTPUT_PATHS="./src/chery.alexsab.ru/data/cars.json"
@@ -10,5 +10,3 @@ export DEALERPRICE='dealer_price.json'
 export DEALERPRICEFIELD='Конечная цена'
 export DEALERBENEFITFIELD='Скидка'
 node .github/scripts/scrape.js
-
-/html/body/main/div[2]/div/div/div[1]
