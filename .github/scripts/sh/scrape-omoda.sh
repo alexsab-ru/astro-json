1c752ed90e97ac4508e3d3922f23dc5ce48ce3c8
Alexander Sabanov <alexsab@mail.ru>
2025-04-30 08:59:31 +0400

    upd omoda

--- .github/scripts/sh/scrape-omoda.sh
+++ .github/scripts/sh/scrape-omoda.sh
@@ -1,8 +1,8 @@
 export BRAND='omoda'
 export URL="https://omoda.ru/models/"
 export ITEM_XPATH="//li[contains(concat(' ', normalize-space(@class), ' '), ' menu-models__item ')]"
-export ID_XPATH="concat('omoda-', substring-before(substring-after(.//a[contains(@class, 'menu-models__item-link')]/@href, '/models/model'), '/'))"
-export MODEL_XPATH="substring-before(substring-after(.//a[contains(@class, 'menu-models__item-link')]/@href, '/models/model'), '/')"
+export ID_XPATH="concat('omoda-', substring-before(substring-after(.//a[contains(@class, 'menu-models__item-link')]/@href, '/models/'), '/'))"
+export MODEL_XPATH=".//div[contains(@class, 'menu-models__item-title')]/text()"
 export PRICE_XPATH="translate(string(.//div[contains(@class, 'menu-models__item-price')]/text()), translate(string(.//div[contains(@class, 'menu-models__item-price')]/text()), '0123456789', ''), '')"
 export LINK_XPATH=".//a[contains(@class, 'menu-models__item-link')]/@href"
 export OUTPUT_PATHS="./src/omoda-ulyanovsk.ru/data/cars.json,./src/omoda.alexsab.ru/data/cars.json"
