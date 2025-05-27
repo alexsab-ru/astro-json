74ea437d0893701d84fbacbf6ab851949baa8d33
Oksana T. <pochtoviy-yashik@mail.ru>
2025-05-27 16:28:44 +0400

    add paths

--- .github/scripts/sh/extract-baic.sh
+++ .github/scripts/sh/extract-baic.sh
@@ -1,7 +1,7 @@
 export BRAND='baic'
 export URL='http://baic-auto.ru/models/'
 export REGEXP='\$\{JSON\.stringify\((\{"menuType":.*)\).replace\('
-export OUTPUT_PATHS="./src/baic-alpha.ru/data/cars.json,./src/baic-krasnodar.ru/data/cars.json,./src/baic-samara.ru/data/cars.json"
+export OUTPUT_PATHS="./src/baic-alpha.ru/data/cars.json,./src/baic-krasnodar.ru/data/cars.json,./src/baic-samara.ru/data/cars.json,./src/baic.alexsab.ru/data/cars.json"
 export DEALERPRICE='dealer_price.json'
 export DEALERPRICEFIELD='Конечная цена'
 export DEALERBENEFITFIELD='Скидка'
--- .github/scripts/sh/extract-kaiyi.sh
+++ .github/scripts/sh/extract-kaiyi.sh
@@ -1,7 +1,7 @@
 export BRAND='kaiyi'
 export URL='https://kaiyi-auto.ru/models/'
 export REGEXP='\$\{JSON\.stringify\((\{"menuType":.*)\).replace\('
-export OUTPUT_PATHS="./src/kaiyi-alpha.ru/data/cars.json,./src/kaiyi-krd.ru/data/cars.json,./src/kaiyi-samara.ru/data/cars.json"
+export OUTPUT_PATHS="./src/kaiyi-alpha.ru/data/cars.json,./src/kaiyi-krd.ru/data/cars.json,./src/kaiyi-samara.ru/data/cars.json,./src/kaiyi.alexsab.ru/data/cars.json"
 export DEALERPRICE='dealer_price.json'
 export DEALERPRICEFIELD='Конечная цена'
 export DEALERBENEFITFIELD='Скидка'
