ef4c48ae20d84ca136be114788142eaf4bc134a3
Alexander Sabanov <alexsab@mail.ru>
2025-06-06 10:55:44 +0400

    add atkmotors-vostok.ru to scrape

--- .github/scripts/sh/mergeJson.sh
+++ .github/scripts/sh/mergeJson.sh
@@ -1,3 +1,7 @@
 export INPUT_PATHS="./tmp/auto-team.pro/data/belgee.json,./tmp/auto-team.pro/data/geely.json,./tmp/auto-team.pro/data/gac.json,./tmp/auto-team.pro/data/changan.json,./tmp/auto-team.pro/data/knewstar.json"
 export OUTPUT_PATHS="./src/auto-team.pro/data/cars.json"
 node .github/scripts/mergeJson.js
+
+export INPUT_PATHS="./tmp/atkmotors-vostok.ru/data/chery.json,./tmp/atkmotors-vostok.ru/data/changan.json"
+export OUTPUT_PATHS="./src/atkmotors-vostok.ru/data/cars.json"
+node .github/scripts/mergeJson.js
--- .github/scripts/sh/scrape-changan.sh
+++ .github/scripts/sh/scrape-changan.sh
@@ -5,5 +5,5 @@ export ID_XPATH="concat('changan-', substring-before(substring-after(@href, '/mo
 export MODEL_XPATH=".//span[contains(@class,'card__info-title')]/text()"
 export PRICE_XPATH="translate(string(.//span[contains(@class,'card__info-title')]/following-sibling::span[1]/text()), translate(string(.//span[contains(@class,'card__info-title')]/following-sibling::span[1]/text()), '0123456789', ''), '')"
 export LINK_XPATH="./@href"
-export OUTPUT_PATHS="./tmp/auto-team.pro/data/changan.json"
+export OUTPUT_PATHS="./tmp/auto-team.pro/data/changan.json,./tmp/atkmotors-vostok.ru/data/changan.json"
 node .github/scripts/scrape.js
