61c170917ca3670a970b682acd50e6deb289a5e0
Alexander Sabanov <alexsab@mail.ru>
2025-06-23 13:03:43 +0400

    return "new-" and "Новый " to haval
    Ошибка, там новые тачки и их никак не отличить, если нет приписки

--- .github/scripts/sh/scrape-haval.sh
+++ .github/scripts/sh/scrape-haval.sh
@@ -1,8 +1,8 @@
 export BRAND='haval'
 export URL="https://haval.ru/models/"
 export ITEM_XPATH="//li[contains(concat(' ', normalize-space(@class), ' '), ' web_block_models__item ')]"
-export ID_XPATH="concat('haval-', replace(substring-before(substring-after((.//div[contains(@class, 'web_block_models__item-btn')]/a/@href)[1], '/models/'), '/'), '(/models/|haval-|new-|/$)', ''))"
-export MODEL_XPATH="replace((.//div[contains(concat(' ', normalize-space(@class), ' '), ' web_block_models__item-title ')]/text()), 'НОВЫЙ ', '')"
+export ID_XPATH="concat('haval-', replace(substring-before(substring-after((.//div[contains(@class, 'web_block_models__item-btn')]/a/@href)[1], '/models/'), '/'), '(haval-)', ''))"
+export MODEL_XPATH="replace((.//div[contains(concat(' ', normalize-space(@class), ' '), ' web_block_models__item-title ')]/text()), 'НОВЫЙ1 ', '')"
 export PRICE_XPATH="translate(string(.//div[contains(@class, 'web_block_models__item-price')]), translate(string(.//div[contains(@class, 'web_block_models__item-price')]), '0123456789', ''), '')"
 export LINK_XPATH=".//div[contains(@class, 'web_block_models__item-btn')]/a/@href"
 export OUTPUT_PATHS="./src/haval-ulyanovsk.ru/data/cars.json,./src/haval.alexsab.ru/data/cars.json"
