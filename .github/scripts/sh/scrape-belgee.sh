c1025c7eb63bc79712f996b328e0b9e205beac18
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-07-29 10:44:30 +0400

    Update scrape-belgee.sh

--- .github/scripts/sh/scrape-belgee.sh
+++ .github/scripts/sh/scrape-belgee.sh
@@ -1,9 +1,9 @@
 export BRAND='belgee'
 export URL="https://belgee.ru"
-export ITEM_XPATH="//header/div[@data-id='models']/div"
+export ITEM_XPATH="//article[@class='model-card']"
 export ID_XPATH="concat('belgee-', substring-after(substring-after(./a/@href, '/model/'), 'belgee-'))"
-export MODEL_XPATH="./a/span[@class='title']/text()"
-export PRICE_XPATH="translate(string(./a/span[@class='subtitle']/text()), translate(string(./a/span[@class='subtitle']/text()), '0123456789', ''), '')"
+export MODEL_XPATH="./div[@class='model-card__logo']/img/@alt"
+export PRICE_XPATH="translate(string(./div[@class='model-card__content']/p/text()), translate(string(./div[@class='model-card__content']/p/text()), '0123456789', ''), '')"
 export LINK_XPATH="./a/@href"
 export OUTPUT_PATHS="./src/belgee-orenburg.ru/data/cars.json,./src/belgee-smr.ru/data/cars.json,./src/belgee-partner-saratov.ru/data/cars.json,./src/belgee-samara.ru/data/cars.json,./tmp/auto-team.pro/data/belgee.json,./src/belgee.alexsab.ru/data/cars.json"
 export DEALERPRICE='dealer_price.json'
