760a9127252de3386aedb85a359cf33d53f4d070
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-08-03 18:45:39 +0400

    test scrape tank

--- .github/scripts/sh/scrape-soueast.sh
+++ .github/scripts/sh/scrape-soueast.sh
@@ -5,7 +5,7 @@ export ID_XPATH="concat('soueast-', substring-after(.//div[contains(@class, 'td-
 export MODEL_XPATH=".//div[contains(@class, 'td-model-card__head')]//div[contains(@class, 'td-model-card__title')]"
 export PRICE_XPATH="translate(string(.//div[contains(@class, 'td-model-card__price-wrap')]/div/text()[1]), translate(string(.//div[contains(@class, 'td-model-card__price')]/div/text()[1]), '0123456789', ''), '')"
 export LINK_XPATH=".//div[contains(@class, 'td-model-card__btns')]/a/@href"
-export ITEM_CSS="div[class='td-models-grid__item"
+export ITEM_CSS="div[class='td-models-grid__item']"
 export MODEL_CSS="span[class='td-model-card__name']"
 export PRICE_CSS="div[class='td-model-card__price']"
 export LINK_CSS="a[class*='td-btn td-btn-tertiary']"
