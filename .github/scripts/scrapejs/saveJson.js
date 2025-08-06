068f0a3fff75ff0f8c054c7a6242e826506aeaf9
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-08-06 12:50:53 +0400

    Create copy cars.json -> federal-models_price.json

--- .github/scripts/scrapejs/saveJson.js
+++ .github/scripts/scrapejs/saveJson.js
@@ -1,14 +1,32 @@
 const fs = require('fs');
+const path = require('path');
 
 const WriteFileConfig = {
   JSON_INDENT_SIZE: 2,
   CHARSET: 'utf-8'
 };
 
+const FileName = {
+  ORIGINAL: 'cars.json',
+  COPY: 'federal-models_price.json'
+};
+
+const checkDirectory = (filePath) => {
+  const dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  fs.mkdirSync(dirname, { recursive: true });
+};
+
 const saveJson = (data, paths) => {
   for (const path of paths) {
+    checkDirectory(path);
     console.log('Сохраняю данные по пути: ', path);
     fs.writeFileSync(path, JSON.stringify(data, null, WriteFileConfig.JSON_INDENT_SIZE), WriteFileConfig.CHARSET);
+    const copyPath = path.replace(FileName.ORIGINAL, FileName.COPY);
+    console.log(`Создаю копию файла: ${copyPath}`);
+    fs.copyFileSync(path, copyPath);
   }
 };
 
