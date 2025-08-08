3d0c4d9031719023665adff51630b63007ce22b7
Alexander Sabanov <alexsab@mail.ru>
2025-08-08 23:44:08 +0400

    remove timestamp from log

--- .github/scripts/scrapejs/logError.js
+++ .github/scripts/scrapejs/logError.js
@@ -4,7 +4,8 @@ const ERROR_FILE_NAME = 'output.txt';
 
 const logError = (error) => {
   const timestamp = new Date().toISOString();
-  const errorMessage = `[${timestamp}] ${error}\n`;
+  let errorMessage = `[${timestamp}] `;
+  errorMessage = `${error}\n`;
 
   try {
     fs.appendFileSync(ERROR_FILE_NAME, errorMessage);
