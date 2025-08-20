92ad90d81bcecc88d43efc417896e72036ac46ab
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-08-20 12:17:44 +0400

    Update scrape.js

--- .github/scripts/scrapejs/scrape.js
+++ .github/scripts/scrapejs/scrape.js
@@ -9,24 +9,27 @@ const TIMEOUT_BETWEEN_RETRIES = 2000;
 const OUTPUT_PATHS = process.env.OUTPUT_PATHS.split(',');
 const Message = {
   SUCCESS: 'Данные успешно сохранены!',
-  ERROR: 'Ошибка получения данных: '
+  ERROR: 'Ошибка получения данных. '
 };
+const brand = process.env.BRAND;
+const url = process.env.URL;
 
 (async () => {
   let data;
   let retryCount = 0;
 
+  console.log(`Сайт ${brand.toUpperCase()} ${url}`);
   while (retryCount <= MAX_RETRIES) {
     console.log(`Попытка получения данных ${retryCount + 1} из ${MAX_RETRIES} ...`);
     try {
       data = await getElements();
       break;
     } catch (err) {
-      console.error(`Попытка #${retryCount + 1} не удалась:`, err.message);
+      console.error(`Попытка #${retryCount + 1} не удалась.`, err.message);
       retryCount++;
       
       if (retryCount === MAX_RETRIES) {
-        throw new Error(`\nНе удалось получить данные ${err.message}`);
+        throw new Error(`\nНе удалось получить данные ${brand.toUpperCase()}. ${err.message}`);
       }
       
       await new Promise(resolve => setTimeout(resolve, TIMEOUT_BETWEEN_RETRIES));
@@ -44,5 +47,3 @@ const Message = {
   logError(err.message);
   process.exit(0);
 });
-
-
