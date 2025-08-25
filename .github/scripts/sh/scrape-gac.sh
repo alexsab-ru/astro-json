cdc703a79a66535b8b4a4d610a9ea627afbc36e0
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-08-25 14:32:23 +0400

    Update scrape-gac.sh

--- .github/scripts/sh/scrape-gac.sh
+++ .github/scripts/sh/scrape-gac.sh
@@ -1,5 +1,5 @@
 export BRAND='gac'
-export URL="https://gac.ru/models"
+export URL="https://models.gac.alexsab.ru"
 export ITEM_XPATH="//div[@class='td-models-grid__item']"
 export ID_XPATH="concat('gac-', substring-after(.//a[starts-with(@href, '/models/')]/@href, '/models/'))"
 export MODEL_XPATH="substring-after(.//a[starts-with(@href, '/models/')]/@href, '/models/')"
@@ -10,5 +10,5 @@ export MODEL_CSS="img"
 export PRICE_CSS="div[class='td-model-card__price'] span"
 export LINK_CSS="a[class*='td-btn-primary']"
 export OUTPUT_PATHS="./src/gac.alexsab.ru/data/cars.json,./src/gac-smr.alexsab.ru/data/cars.json,./src/gac-orenburg.alexsab.ru/data/cars.json,./src/gac-stavauto.alexsab.ru/data/cars.json,./src/gac-partner-samara.alexsab.ru/data/cars.json,./src/gac-samara.ru/data/cars.json,./src/gac-armada-avto.ru/data/cars.json,./tmp/auto-team.pro/data/gac.json"
-python3 .github/scripts/scrape.py
-# node .github/scripts/scrapejs/scrape.js
+# python3 .github/scripts/scrape.py
+node .github/scripts/scrapejs/scrape.js
