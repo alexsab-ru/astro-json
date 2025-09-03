1b7ef6ac596e0a8fa7055d7b41a9aafbf9226767
Alexander Sabanov <alexsab@mail.ru>
2025-09-04 03:00:42 +0400

    Update scrape-knewstar.sh

--- .github/scripts/sh/scrape-knewstar.sh
+++ .github/scripts/sh/scrape-knewstar.sh
@@ -1,9 +1,9 @@
 export BRAND='knewstar'
 export URL="https://knewstar.ru/"
-export ITEM_XPATH="//a[contains(concat(' ', normalize-space(@class), ' '), ' v2-menu-curtain-automobiles__item ')]"
-export ID_XPATH="concat('', substring-after(./@href, '/model/'))"
-export MODEL_XPATH=".//h4[contains(concat(' ', normalize-space(@class), ' '), ' v2-automobile-menu-card__heading ')]"
-export PRICE_XPATH="translate(string(.//p[contains(@class, 'v2-automobile-menu-card__paragraph')]/text()), translate(string(.//p[contains(@class, 'v2-automobile-menu-card__paragraph')]/text()), '0123456789', ''), '')"
+export ITEM_XPATH="//a[contains(@class,'menu-models__item-link')]"
+export ID_XPATH="concat('', substring-before(substring-after(./@href, '/models/'),'/'))"
+export MODEL_XPATH=".//div[contains(@class, 'menu-models__item-title')]"
+export PRICE_XPATH="translate(string(.//div[contains(@class, 'menu-models__item-price')]/text()), translate(string(.//div[contains(@class, 'menu-models__item-price')]/text()), '0123456789', ''), '')"
 export LINK_XPATH="./@href"
 export OUTPUT_PATHS="./src/knewstar.alexsab.ru/data/cars.json"
 node .github/scripts/scrape.js
