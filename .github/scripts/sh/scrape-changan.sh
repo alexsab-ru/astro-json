cb5274d17d3dbd97a7e0fc2d653a6bd411bbfb81
Alexander Sabanov <alexsab@mail.ru>
2025-09-04 01:53:16 +0400

    fix scrape-changan

--- .github/scripts/sh/scrape-changan.sh
+++ .github/scripts/sh/scrape-changan.sh
@@ -1,9 +1,9 @@
 export BRAND='changan'
-export URL="https://changanauto.ru/models/"
-export ITEM_XPATH="//a[contains(@class,'card_bg-hover')]"
-export ID_XPATH="concat('changan-', substring-before(substring-after(@href, '/models/'),'/'))"
-export MODEL_XPATH=".//span[contains(@class,'card__info-title')]/text()"
-export PRICE_XPATH="translate(string(.//span[contains(@class,'card__info-title')]/following-sibling::span[1]/text()), translate(string(.//span[contains(@class,'card__info-title')]/following-sibling::span[1]/text()), '0123456789', ''), '')"
-export LINK_XPATH="./@href"
+export URL="https://changanauto.ru/model"
+export ITEM_XPATH="//div[@class='flex flex-col justify-between']"
+export ID_XPATH="concat('changan-', substring-after(.//a/@href, '/model/'))"
+export MODEL_XPATH=".//div[contains(@class,'text-subheading-bold-24')]/text()"
+export PRICE_XPATH="translate(string(.//div[contains(@class,'text-simple-14')]/text()), translate(string(.//div[contains(@class,'text-simple-14')]/text()), '0123456789', ''), '')"
+export LINK_XPATH=".//a/@href"
 export OUTPUT_PATHS="./src/changan.alexsab.ru/data/cars.json"
 node .github/scripts/scrape.js
