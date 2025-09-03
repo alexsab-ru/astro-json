678d73dce0c165a2a30f8a06582e972a262f76b7
Alexander Sabanov <alexsab@mail.ru>
2025-09-03 21:31:41 +0400

    fix scrape-belgee

--- .github/scripts/sh/scrape-belgee.sh
+++ .github/scripts/sh/scrape-belgee.sh
@@ -3,7 +3,7 @@ export URL="https://belgee.ru"
 export ITEM_XPATH="//header//li[@class='menu-models__item']"
 export ID_XPATH="concat('belgee-', substring-before(substring-after(substring-after(.//a[contains(@class,'menu-models__item-btn')]/@href, '/model/'), 'belgee-'), '/'))"
 export MODEL_XPATH=".//div[@class='menu-models__item-title h3']/text()"
-export PRICE_XPATH="translate(string(.//div[@class='menu-models__item-price caps mb-green']/text()), translate(string(.//div[@class='menu-models__item-price caps mb-green']/text()), '0123456789', ''), '')"
+export PRICE_XPATH="translate(string(.//div[contains(@class,'menu-models__item-price')]/text()), translate(string(.//div[contains(@class,'menu-models__item-price')]/text()), '0123456789', ''), '')"
 export LINK_XPATH=".//a[@class='btn btn_secondary menu-models__item-btn']/@href"
 export OUTPUT_PATHS="./src/belgee-orenburg.ru/data/cars.json,./src/belgee-smr.ru/data/cars.json,./src/belgee-partner-saratov.ru/data/cars.json,./src/belgee-samara.ru/data/cars.json,./src/belgee.alexsab.ru/data/cars.json"
 node .github/scripts/scrape.js
