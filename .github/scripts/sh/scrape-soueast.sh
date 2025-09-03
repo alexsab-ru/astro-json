0590428b9b7afd28152abe92e0677ec48afff4db
Alexander Sabanov <alexsab@mail.ru>
2025-09-04 03:07:46 +0400

    Update scrape-soueast.sh

--- .github/scripts/sh/scrape-soueast.sh
+++ .github/scripts/sh/scrape-soueast.sh
@@ -3,7 +3,7 @@ export URL="https://soueast.ru/block?key=obertka-dlya-modelnyj-ryad-animirovanny
 export ITEM_XPATH="//div[contains(@class, 'td-model-card__content')]"
 export ID_XPATH="concat('soueast-', substring-after(.//div[contains(@class, 'td-model-card__btns')]/a/@href, '/'))"
 export MODEL_XPATH=".//div[contains(@class, 'td-model-card__head')]//div[contains(@class, 'td-model-card__title')]"
-export PRICE_XPATH="translate(string(.//div[contains(@class, 'td-model-card__price-wrap')]/div/text()[1]), translate(string(.//div[contains(@class, 'td-model-card__price')]/div/text()[1]), '0123456789', ''), '')"
+export PRICE_XPATH="translate(string(.//div[contains(@class, 'td-model-card__price')]/text()[1]), translate(string(.//div[contains(@class, 'td-model-card__price')]/text()[1]), '0123456789', ''), '')"
 export LINK_XPATH=".//div[contains(@class, 'td-model-card__btns')]/a/@href"
 export OUTPUT_PATHS="./src/soueast.alexsab.ru/data/cars.json"
 node .github/scripts/scrape.js
