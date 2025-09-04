32a0578b6299ca87009fc322cd73c23c1d453aa4
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-09-04 11:36:11 +0400

    Update scrape-changan.sh

--- .github/scripts/sh/scrape-changan.sh
+++ .github/scripts/sh/scrape-changan.sh
@@ -7,7 +7,7 @@ export PRICE_XPATH="translate(string(.//div[contains(@class,'text-simple-14')]/t
 export LINK_XPATH=".//a/@href"
 export ITEM_CSS="div[class='flex flex-col justify-between']"
 export MODEL_CSS="div[class='text-subheading-bold-24 lg:text-subheading-bold-32 mb-2 text-center']"
-export PRICE_CSS="div[class='text-center text-simple-14 lg:text-simple-16 mb-4 lg:mb-6"
+export PRICE_CSS="div[class='text-center text-simple-14 lg:text-simple-16 mb-4 lg:mb-6']"
 export LINK_CSS="a"
 
 export OUTPUT_PATHS="./src/changan.alexsab.ru/data/cars.json"
