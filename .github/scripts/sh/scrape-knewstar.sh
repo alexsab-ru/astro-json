58ec4e9e1bc24d269360784f6911ec31224836fa
psdhtmlcss <pochtoviy-yashik@mail.ru>
2025-09-04 12:09:02 +0400

    Update scrape-knewstar.sh

--- .github/scripts/sh/scrape-knewstar.sh
+++ .github/scripts/sh/scrape-knewstar.sh
@@ -1,16 +1,17 @@
 export BRAND='knewstar'
 export URL="https://knewstar.ru/"
-export ITEM_XPATH="//a[contains(concat(' ', normalize-space(@class), ' '), ' v2-menu-curtain-automobiles__item ')]"
-export ID_XPATH="concat('', substring-after(./@href, '/model/'))"
-export MODEL_XPATH=".//h4[contains(concat(' ', normalize-space(@class), ' '), ' v2-automobile-menu-card__heading ')]"
-export PRICE_XPATH="translate(string(.//p[contains(@class, 'v2-automobile-menu-card__paragraph')]/text()), translate(string(.//p[contains(@class, 'v2-automobile-menu-card__paragraph')]/text()), '0123456789', ''), '')"
+export ITEM_XPATH="//a[contains(@class,'menu-models__item-link')]"
+export ID_XPATH="concat('', substring-before(substring-after(./@href, '/models/'),'/'))"
+export MODEL_XPATH=".//div[contains(@class, 'menu-models__item-title')]"
+export PRICE_XPATH="translate(string(.//div[contains(@class, 'menu-models__item-price')]/text()), translate(string(.//div[contains(@class, 'menu-models__item-price')]/text()), '0123456789', ''), '')"
 export LINK_XPATH="./@href"
-export ITEM_CSS="a[class*='v2-menu-curtain-automobiles__item']"
-export MODEL_CSS="h4[class='v2-automobile-menu-card__heading']"
-export PRICE_CSS="p[class='v2-automobile-menu-card__paragraph']"
-export LINK_CSS="a[class*='v2-automobile-menu-card']"
-export OUTPUT_PATHS="./src/knewstar.alexsab.ru/data/cars.json,./tmp/auto-team.pro/data/knewstar.json"
-export DEALERPRICE='dealer_price.json'
-export DEALERPRICEFIELD='Конечная цена'
-export DEALERBENEFITFIELD='Скидка'
+
+export ITEM_CSS="div[class='menu-models__item']"
+export MODEL_CSS="div[class*='menu-models__item-title']"
+export PRICE_CSS="div[class*='menu-models__item-price']"
+export LINK_CSS="a"
+
+export OUTPUT_PATHS="./src/knewstar.alexsab.ru/data/cars.json"
+
+# node .github/scripts/scrape.js
 node .github/scripts/scrapejs/scrape.js
