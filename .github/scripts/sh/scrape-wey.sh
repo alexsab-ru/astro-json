0a43c07f5a9670b6493ed232855e8ae0b74d6042
Alexander Sabanov <alexsab@mail.ru>
2026-02-05 00:58:16 +0400

    upd scrape WEY

--- .github/scripts/sh/scrape-wey.sh
+++ .github/scripts/sh/scrape-wey.sh
@@ -1,5 +1,6 @@
 export BRAND='wey'
-export URL="https://gwm-wey.ru/configurator"
+# export URL="https://gwm-wey.ru/configurator"
+export URL="https://models.wey.alexsab.ru/configurator"
 export ITEM_XPATH="//div[contains(concat(' ', normalize-space(@class), ' '), ' main-navigation-widgets__car ')]"
 export ID_XPATH=".//article[@class='main-navigation-widget-card__title']/text()"
 export MODEL_XPATH=".//article[@class='main-navigation-widget-card__title']/text()"
@@ -8,6 +9,6 @@ export LINK_XPATH="concat('/models/', substring-after(.//a[starts-with(@href, '/
 export OUTPUT_PATHS="./src/wey.alexsab.ru/data/cars.json"
 export CLICK_SELECTOR="#__nuxt > div > div.navigation-header > div > div.main-navigation-menu.flex.navigation-header__menu-left > div:nth-child(2)"
 export WAIT_SELECTOR="#__nuxt > div > div.navigation-header > div > div.main-navigation-menu.flex.navigation-header__menu-left > div:nth-child(2)"
-export WAIT_TIME=1
+export WAIT_TIME=1000
 # python3 .github/scripts/scrape.py
 node .github/scripts/scrape.js
